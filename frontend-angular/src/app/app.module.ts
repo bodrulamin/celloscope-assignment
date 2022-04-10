fc8f3c309589fe0166175163ede7d903460ac60a
bodrulamin <bodrulaminiu@gmail.com>
2022-04-10 17:55:49 +0600

    forgot component added

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotComponent } from './forgot/forgot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
