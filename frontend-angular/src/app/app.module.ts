636e8b4064edee5a426507be018983b5177624ec
bodrulamin <bodrulaminiu@gmail.com>
2022-04-10 18:53:04 +0600

    register component added

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotComponent } from './forgot/forgot.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ForgotComponent } from './forgot/forgot.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-angular/src/app/register/register.component.ts
+++ frontend-angular/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  fg: any
   constructor() { }
 
   ngOnInit(): void {
+    this.fg = new FormGroup({
+      userId: new FormControl([Validators.required]),
+      mobile: new FormControl([Validators.required]),
+      password: new FormControl([Validators.required])
+    })
+  }
+  register() {
+    this.fg.markAllAsTouched();
+
   }
 
+  ifError(con: string) {
+    return this.fg.get(con).invalid && this.fg.get(con).touched;
+  }
 }
